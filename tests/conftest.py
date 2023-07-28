fa3e569f85eecefcd5804f38d9b51a3579d2a858
Danila Panov <danila.panov.02@mail.ru>
2023-07-28 16:58:42 +0600

    fixes problems

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,9 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.base_url = 'https://demoqa.com'
+def browser_open():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
-
-    yield
-
-    browser.quit()
+    browser.config.base_url = 'https://demoqa.com'
